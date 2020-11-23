92c29db7ccd88d5b127d70232d40880d915dda1b
PAP0 <patryk.podworny@gmail.com>
2020-11-23 13:14:24 +0100

    Begin aan movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    #region Variables
+
+    public Rigidbody2D PlayerRb;
+    public float speed;
+    public float jumpForce;
+
+    #endregion
+
+    #region Functions
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        PlayerRb.velocity = new Vector2(moveBy, PlayerRb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerRb.velocity = new Vector2(PlayerRb.velocity.x, jumpForce);
+        }
+    }
+
+    #endregion
+
+    void Start()
+    {   
+        PlayerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+}
