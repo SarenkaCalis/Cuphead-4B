9f5d889fa7d1b46688746820a40e88abb49a0643
Milan Ligtermoet <28470@ma-web.nl>
2020-11-30 12:12:09 +0100

    Player HP. Tijdelijke key for damage is even op spatie. Als je spatie klikt gaat er 1 HP van af.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] Text hptext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+        hptext = GetComponentInParent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hptext.text = "HP: " + health;
+    }
+    public void TakeDamage()
+    {
+        health += -1;
+    }
+}
